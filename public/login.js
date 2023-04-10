bd05891fa6f490957fce7450d663be4a0d18dd9c
Matthew Lund <lundmatthewj@gmail.com>
2023-04-09 19:38:24 -0600

    The login screen is back!

--- public/login.js
+++ public/login.js
@@ -1,83 +1,78 @@
-// (async () => {
-//     let authenticated = false;
-//     const userName = localStorage.getItem('userName');
-//     if (userName) {
-//       const nameEl = document.querySelector('#userName');
-//       nameEl.value = userName;
-//       const user = await getUser(nameEl.value);
-//       authenticated = user?.authenticated;
-//     }
-  
-//     if (authenticated) {
-//       document.querySelector('#playerName').textContent = userName;
-//       setDisplay('loginControls', 'none');
-//       setDisplay('playControls', 'block');
-//     } else {
-//       setDisplay('loginControls', 'block');
-//       setDisplay('playControls', 'none');
-//     }
-//   })();
-  
-// async function loginUser() {
-//   loginOrCreate(`/api/auth/login`);
-// }
+(async () => {
+  let authenticated = false;
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    const nameEl = document.querySelector('#userName');
+    nameEl.value = userName;
+    const user = await getUser(nameEl.value);
+    authenticated = user?.authenticated;
+  }
 
-// async function createUser() {
-//   loginOrCreate(`/api/auth/create`);
-// }
+  if (authenticated) {
+    document.querySelector('#playerName').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
 
-// async function loginOrCreate(endpoint) {
-//   const userName = document.querySelector('#userName')?.value;
-//   const password = document.querySelector('#userPassword')?.value;
-//   const response = await fetch(endpoint, {
-//     method: 'post',
-//     body: JSON.stringify({ email: userName, password: password }),
-//     headers: {
-//       'Content-type': 'application/json; charset=UTF-8',
-//     },
-//   });
-//   const body = await response.json();
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
 
-//   if (response?.status === 200) {
-//     localStorage.setItem('userName', userName);
-//     window.location.href = 'play.html';
-//   } else {
-//     const modalEl = document.querySelector('#msgModal');
-//     modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
-//     const msgModal = new bootstrap.Modal(modalEl, {});
-//     msgModal.show();
-//   }
-// }
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
 
-// function play() {
-//   window.location.href = 'play.html';
-// }
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const body = await response.json();
 
-// function logout() {
-//   fetch(`/api/auth/logout`, {
-//     method: 'delete',
-//   }).then(() => (window.location.href = '/'));
-// }
+  if (response?.status === 200) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'play.html';
+  } else {
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
 
-// async function getUser(email) {
-//   // See if we have a user with the given email.
-//   const response = await fetch(`/api/user/${email}`);
-//   if (response.status === 200) {
-//     return response.json();
-//   }
+function play() {
+  window.location.href = 'play.html';
+}
 
-//   return null;
-// }
+function logout() {
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
 
-// function setDisplay(controlId, display) {
-//   const playControlEl = document.querySelector(`#${controlId}`);
-//   if (playControlEl) {
-//     playControlEl.style.display = display;
-//   }
-// }
+async function getUser(email) {
+  let scores = [];
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
 
-function login() {
-  const nameEl = document.querySelector("#userName");
-  localStorage.setItem("userName", nameEl.value);
-  window.location.href = "rolld20.html";
+  return null;
+}
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
 }
