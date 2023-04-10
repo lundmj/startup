68267fd48217add4124d3db3722f7469f253943f
Matthew Lund <lundmatthewj@gmail.com>
2023-04-09 19:34:38 -0600

    Added user creation and storage abilities

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const {MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -7,10 +9,33 @@ const hostname = process.env.MONGOHOSTNAME;
 const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
-const pastPlayers = client.db('DD').collection('pastPlayers');
+const userCollection = client.db('DD').collection('users');
 
-function addPlayer(player){
-    pastPlayers.insertOne(player);
+/*USER FUNCTIONALITY*/
+function getUser(userName) {
+    return userCollection.findOne({ userName: userName });
 }
-
-module.exports = {addPlayer};
+  
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+  
+async function createUser(userName, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      userName: userName,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+}
+  
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
+  };
--- index.js
+++ index.js
@@ -1,23 +1,98 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('database.js');
 
-// The service port. In production the front-end code is statically hosted by the service on the same port.
+const authCookieName = 'token';
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-// JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Serve up the front-end static content hosting
+app.use(cookieParser());
+
 app.use(express.static('public'));
 
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.userName)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await DB.createUser(req.body.userName, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
+    }
+});
+  
+  // GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.userName);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+  
+  // DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+  
+  // GetUser returns information about a user
+apiRouter.get('/user/:userName', async (req, res) => {
+    const user = await DB.getUser(req.params.userName);
+    if (user) {
+      const token = req?.cookies.token;
+      res.send({ userName: user.userName, authenticated: token === user.token });
+      return;
+    }
+    res.status(404).send({ msg: 'Unknown' });
+});
+  
+  // secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+  
+secureApiRouter.use(async (req, res, next) => {
+    authToken = req.cookies[authCookieName];
+    const user = await DB.getUserByToken(authToken);
+    if (user) {
+      next();
+    } else {
+      res.status(401).send({ msg: 'Unauthorized' });
+    }
+});
+
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+  });
+
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
   
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+}
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
